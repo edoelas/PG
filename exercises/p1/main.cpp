1bbb6d490ea24fd856586a284ef0c9991205d0da
edoelas <miego.cvcv@gmail.com>
2024-02-29 20:59:24 +0100

    p1 wip

--- exercises/p1/main.cpp
+++ exercises/p1/main.cpp
@@ -2,6 +2,7 @@
 #include <PGUPV.h>
 #include <GUI3.h>
 #include <iomanip>
+#include "glmReader.h"
 
 using namespace PGUPV;
 
@@ -17,6 +18,8 @@ private:
 	void buildGUI();
 	std::shared_ptr<GLMatrices> mats;
 	std::unique_ptr<PGUPV::Mesh> boundary;
+	std::unique_ptr<PGUPV::Mesh> exteriorMesh;
+	std::unique_ptr<PGUPV::Mesh> interiorMesh;
 	std::shared_ptr<Label> cursorPos;
 	glm::uvec2 windowSize{ 0 };
 
@@ -57,26 +60,72 @@ bool MyRender::mouse_move(const MouseMotionEvent& me) {
 	return false;
 }
 
-void MyRender::setup() {
+void MyRender::setup() {    
 	glClearColor(0.6f, 0.6f, 0.9f, 1.0f);
 	mats = GLMatrices::build();
 
 	buildGUI();
+	std::string fname = App::assetsDir() + "/data/A.ES.SDGC.BU.46900.buildingpart.gml";
+	City city = readBuildings(fname, true);
 
-	// Creamos un cuadrado para que la escena no esté vacía. Lo puedes usar para mostrar
-	// los límites de la ciudad.
+
+	exteriorMesh = std::make_unique<Mesh>();
+	interiorMesh = std::make_unique<Mesh>();
+
+	std::vector<glm::vec3> exteriorVertices;
+	std::vector<GLint> exteriorFirst;
+	std::vector<GLsizei> exteriorCount;
+	int exteriorCounter = 0;
+
+	std::vector<glm::vec3> interiorVertices;
+	std::vector<GLint> interiorFirst;
+	std::vector<GLsizei> interiorCount;
+	int interiorCounter = 0;
+
+	for (const Building& building : city.buildings) {
+		for (const BuildingPart& part : building.parts) {
+			// exterior
+			exteriorFirst.push_back(exteriorCounter);
+			for (const glm::dvec2& exterior : part.exterior) {
+				float x = static_cast<float>(exterior.x);
+				float y = static_cast<float>(exterior.y);
+				exteriorVertices.push_back(glm::vec3(x, y, 0.0f));
+				exteriorCounter++;
+
+			}
+			exteriorCount.push_back(exteriorCounter - exteriorFirst.back());
+			
+			// interior
+			for (const auto interior : part.interior) {
+			interiorFirst.push_back(interiorCounter);
+				for (const glm::dvec2& hole : interior) {
+					float x = static_cast<float>(hole.x);
+					float y = static_cast<float>(hole.y);
+					interiorVertices.push_back(glm::vec3(x, y, 0.0f));
+					interiorCounter++;
+				}
+			interiorCount.push_back(interiorCounter - interiorFirst.back());
+			}
+		}
+	}
+
+	exteriorMesh->addVertices(exteriorVertices); 
+	exteriorMesh->addDrawCommand(new MultiDrawArrays(GL_LINE_LOOP, exteriorFirst.data(), exteriorCount.data(), exteriorFirst.size()));
+
+	interiorMesh->addVertices(interiorVertices);
+	interiorMesh->addDrawCommand(new MultiDrawArrays(GL_LINE_LOOP, interiorFirst.data(), interiorCount.data(), interiorFirst.size()));
+
+
+	// Limites
 	boundary = std::make_unique<PGUPV::Mesh>();
-	auto ll = glm::vec2{ -50.f, -50.0f};
-	auto ur = glm::vec2{ 50.f, 50.0f};
-	boundary->addVertices(
-		{ ll,
-		glm::vec2{ur.x, ll.y}, 
-		ur,	
-		glm::vec2{ll.x, ur.y}});
+	auto ll = city.min;
+	auto ur = city.max;
+	boundary->addVertices({ ll, glm::vec2{ur.x, ll.y}, ur, glm::vec2{ll.x, ur.y}});
+	boundary->setColor(glm::vec4{ 0.8f, 0.1f, 0.1f, 1.0f });
 	boundary->addDrawCommand(new PGUPV::DrawArrays(GL_LINE_LOOP, 0, 4));
 
-	setCameraHandler(std::make_shared<XYPanZoomCamera>(
-		1000.0f, glm::vec3{ 0.0f}));
+	auto center = glm::vec3{ (city.min.x + city.max.x) / 2, (city.min.y + city.max.y) / 2, 0.0f };
+	setCameraHandler(std::make_shared<XYPanZoomCamera>(1000.0f, center));
 }
 
 void MyRender::render() {
@@ -86,8 +135,14 @@ void MyRender::render() {
 	mats->setMatrix(GLMatrices::VIEW_MATRIX, getCamera().getViewMatrix());
 
 	ConstantUniformColorProgram::use();
+
 	ConstantUniformColorProgram::setColor(glm::vec4{ 0.8f, 0.1f, 0.1f, 1.0f });
 	boundary->render();
+	ConstantUniformColorProgram::setColor(glm::vec4{ 1.0f, 1.0f, 1.0f, 1.0f });
+	exteriorMesh->render();
+	ConstantUniformColorProgram::setColor(glm::vec4{ 0.8f, 0.1f, 0.1f, 1.0f });
+	interiorMesh->render();
+
 }
 
 
